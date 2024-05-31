13d3bc9d626c01f75b560a42269de74418f373b0
Mokhabadi <mokhabadi@gmail.com>
2024-05-31 16:05:57 +0330

    MainApplicationContext and icons added

---
+++ DNSwitch/MainApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using DNSwitch.Properties;
+using System.Windows.Forms;
+
+namespace DNSwitch
+{
+    internal class MainApplicationContext : ApplicationContext
+    {
+        readonly NotifyIcon notifyIcon;
+
+        public MainApplicationContext()
+        {
+            notifyIcon = new()
+            {
+                Text = "Dextop",
+                Visible = true,
+                Icon = Resources.LightIcon,
+            };
+
+            notifyIcon.DoubleClick += (_, _) => Application.Exit();
+            Application.ApplicationExit += (_, _) => notifyIcon.Visible = false;
+        }
+    }
+}
--- DNSwitch/Program.cs
+++ DNSwitch/Program.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Windows.Forms;
+
 namespace DNSwitch
 {
     internal static class Program
@@ -11,7 +14,7 @@ namespace DNSwitch
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new MainApplicationContext());
         }
     }
 }
