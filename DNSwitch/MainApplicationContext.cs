1721817f0ab8736007a908e4d00090da0c260512
Mokhabadi <mokhabadi@gmail.com>
2024-06-07 21:32:04 +0330

    get list of network adapters and find the internet adapter

--- DNSwitch/MainApplicationContext.cs
+++ DNSwitch/MainApplicationContext.cs
@@ -1,5 +1,12 @@
 ﻿using DNSwitch.Properties;
+using System.Net.NetworkInformation;
+using System;
 using System.Windows.Forms;
+using System.Linq;
+using System.Diagnostics;
+using System.Net;
+using System.Collections.Generic;
+using System.Net.Sockets;
 
 namespace DNSwitch
 {
@@ -17,7 +24,61 @@ namespace DNSwitch
             };
 
             notifyIcon.DoubleClick += (_, _) => Application.Exit();
-            Application.ApplicationExit += (_, _) => notifyIcon.Visible = false;
+            Application.ApplicationExit += ApplicationExit;
+            var res = GetAdapterList();
+            UpdateMenu(res);
+            NetworkChange.NetworkAvailabilityChanged += NetworkAvailabilityChanged;
+            NetworkChange.NetworkAddressChanged += NetworkAddressChanged;
+        }
+
+        void ApplicationExit(object? _, EventArgs __)
+        {
+            notifyIcon.Visible = false;
+            NetworkChange.NetworkAvailabilityChanged -= NetworkAvailabilityChanged;
+            NetworkChange.NetworkAddressChanged -= NetworkAddressChanged;
+        }
+
+        private void NetworkAddressChanged(object? sender, EventArgs e)
+        {
+            Debug.WriteLine("--NetworkAddressChanged");
+            var res = GetAdapterList();
+            UpdateMenu(res);
+        }
+
+        private void NetworkAvailabilityChanged(object? sender, NetworkAvailabilityEventArgs e)
+        {
+            Debug.WriteLine("--NetworkAvailabilityChanged");
+            var res = GetAdapterList();
+            UpdateMenu(res);
+        }
+
+        NetworkInterface[] GetAdapterList()
+        {
+            List<NetworkInterface> adapters = NetworkInterface.GetAllNetworkInterfaces()
+                .Where(
+                x => x.OperationalStatus == OperationalStatus.Up
+                && x.NetworkInterfaceType != NetworkInterfaceType.Tunnel
+                && x.NetworkInterfaceType != NetworkInterfaceType.Loopback
+                ).ToList();
+
+            if (adapters.Count == 1)
+                return [.. adapters];
+
+            var internetAdapter = adapters.FirstOrDefault(x => x.GetIPProperties().GatewayAddresses.Select(x => x.Address).Any(x => x.AddressFamily == AddressFamily.InterNetwork && !x.Equals(IPAddress.Any)));
+
+            if (internetAdapter != null)
+            {
+                adapters.Remove(internetAdapter);
+                adapters.Insert(0, internetAdapter);
+            }
+
+            return [.. adapters];
+        }
+
+        void UpdateMenu(NetworkInterface[] networkInterfaces)
+        {
+            Debug.WriteLine("-UpdateMenu");
+            networkInterfaces.ToList().ForEach(x => Debug.WriteLine(x.Name));
         }
     }
 }
